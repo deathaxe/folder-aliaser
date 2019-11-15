869335adb440bb15b86fcad8ea23b1bfe9654741
DeathAxe <deathaxe@googlemail.com>
2019-11-15 19:20:11 +0100

    Rewrite for ST4 with python 3.8
    This commit basically rewrites the plugin for ST4.
    
    1. Opt-in to run in python 3.8 interpreter
    2. Add InputHandlers for path and alias arguments and run the command
       within the command palette.

--- folder_aliaser.py
+++ folder_aliaser.py
@@ -1,41 +1,107 @@
-import os
+from pathlib import Path
+
+import sublime
 import sublime_plugin
+
+__all__ = [
+    "AliasFolderCommand"
+]
+
+
+def project_folders(project):
+    return (project or {}).get('folders', {})
 
 
 def project_folder(project, path):
-    for folder in (project or {}).get('folders', {}):
+    for folder in project_folders(project):
         if folder['path'] == path:
             return folder
     return None
 
 
-def display_name(project, path):
-    folder = project_folder(project, path)
-    if folder:
-        return folder.get('name') or os.path.basename(folder['path'])
-    return None
+def is_project_folder(project, path):
+    return bool(project_folder(project, path))
+
+
+def display_name(folder):
+    return folder.get('name') or Path(folder['path']).name
+
+
+class PathInputHandler(sublime_plugin.ListInputHandler):
+
+    def __init__(self, folders):
+        self.folders = folders
+
+    def name(self):
+        return "_input_path"
+
+    def placeholder(self):
+        return "Path"
+
+    def list_items(self):
+        return [f"{display_name(folder)}\t{folder['path']}" for folder in self.folders]
+
+    def next_input(self, args):
+        alias, path = args['_input_path'].split('\t')
+        return AliasInputHandler(alias, path)
+
+
+class AliasInputHandler(sublime_plugin.TextInputHandler):
+
+    def __init__(self, alias, path):
+        self.alias = alias
+        self.path = Path(path)
+
+    def name(self):
+        return "alias"
+
+    def placeholder(self):
+        return "Display Name"
+
+    def initial_text(self):
+        return self.alias
+
+    def preview(self, new_alias):
+        if not new_alias:
+            new_alias = self.path.name
+
+        if new_alias != self.alias:
+            hint = f"Change display name from <u>{self.alias}</u> to <u>{new_alias}</u>"
+        else:
+            hint = f"Display name is <u>{self.alias}</u>"
+
+        return sublime.Html(f"{hint}<br><br><i>{self.path}</i>")
 
 
 class AliasFolderCommand(sublime_plugin.WindowCommand):
 
-    def is_enabled(self, dirs):
-        return len(dirs) == 1 and bool(project_folder(self.window.project_data(), dirs[0]))
+    def is_enabled(self, paths=None):
+        return (paths is None or
+                len(paths) == 1 and
+                is_project_folder(self.window.project_data(), paths[0]))
 
-    def run(self, dirs):
-        self.window.show_input_panel(
-            "Alias this folder (enter nothing to clear an alias):",
-            display_name(self.window.project_data(), dirs[0]),
-            lambda alias: self.on_input_panel_submit(alias, dirs[0]),
-            None, None
-        )
+    def is_visible(self, paths=None):
+        return self.is_enabled(paths)
 
-    def on_input_panel_submit(self, alias, path):
+    def input(self, args):
+        if not args.get('paths'):
+            return PathInputHandler(project_folders(self.window.project_data()))
+        elif 'alias' not in args:
+            folder = project_folder(self.window.project_data(), args['paths'][0])
+            return AliasInputHandler(display_name(folder), folder['path'])
+        return None
+
+    def run(self, alias, paths=None, _input_path=None):
+        if _input_path is None:
+            path = Path(paths[0])
+        else:
+            path = Path(_input_path.split('\t')[1])
+
         project = self.window.project_data()
-        folder = project_folder(project, path)
+        folder = project_folder(project, str(path))
         if folder:
-            if alias in ('', os.path.basename(path)):
-                if 'name' in folder:
-                    folder.pop('name')
+            if alias in ('', path.name):
+                folder.pop('name', None)
             else:
                 folder['name'] = alias
             self.window.set_project_data(project)
